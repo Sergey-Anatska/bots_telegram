4a1d4b41dc3005b835c0a2c8dd8f0a3282d65186
Sergiusz Anacka <anackasr@gmail.com>
2023-09-07 14:37:17 +0300

    Added Flaks

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import telebot
 from telebot import types
 from telebot.types import WebAppInfo
+from flask import Flask, request
+import git
+
+app = Flask(__name__)
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 
